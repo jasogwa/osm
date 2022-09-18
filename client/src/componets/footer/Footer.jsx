ab71d7b1adcb866bf9b039507afb1ef3ae907618
Jonas <jonaschinagorom@gmail.com>
2022-09-18 13:36:53 +0200

    footer component

---
+++ client/src/componets/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { 
+    Container,
+    Wapper,
+    Center,
+    Span,CenterBG
+} from './style';
+
+const Footer = ({map,mapCenter}) => {
+  return (
+    <Container>
+      <Wapper>
+        <Center>
+            <CenterBG>Center</CenterBG>
+            <Span>{mapCenter}</Span><br />
+            <CenterBG>Map</CenterBG>
+            <Span>{map}</Span>
+        </Center>
+      </Wapper>
+    </Container>
+  )
+}
+
+export default Footer
---
+++ client/src/componets/footer/style.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 100px;
+    width: 100%;
+    display: block;
+    z-index: 10000;
+    padding: 2px 0px;
+    background-color: #ffffff;
+    opacity: 0.6;
+`;
+
+export const Wapper = styled.div`
+    margin-left: 10px;
+    color: #000000;
+    display: flex;
+`;
+
+export const Center = styled.div`
+    flex: 1;
+`;
+
+export const CenterBG = styled.span`
+    background-color: black;
+    color: white;
+`;
+
+export const Span = styled.span`
+    margin-left: 10px;
+`;
