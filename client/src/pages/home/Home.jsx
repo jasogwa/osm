a132121245e6f95a0e7273916d24ad3ee7d95d1c
Jonas <jonaschinagorom@gmail.com>
2022-09-19 11:58:21 +0200

    readme

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import L from "leaflet";
 import 'leaflet/dist/leaflet.css';
 import osm from "./osm-provider";
