4c0c9fb645c019d9efdb8ef0aed488ebe3b3106d
Jonas <jonaschinagorom@gmail.com>
2022-09-22 13:40:47 +0200

    jest testing in front-end

---
+++ client/src/pages/__tests__/home.test.js
@@ -0,0 +1,8 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import Home from '../home/Home';
+import 'jest-styled-components';
+
+describe(Home, () => {
+    it("true one", () => {
+    });
+});
