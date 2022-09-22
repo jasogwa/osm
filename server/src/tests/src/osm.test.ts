77a85ac4d181c148c17b680472549fd83c306e35
Jonas <jonaschinagorom@gmail.com>
2022-09-22 13:46:12 +0200

    test case

--- server/src/tests/src/osm.test.ts
+++ server/src/tests/src/osm.test.ts
@@ -1,6 +1,19 @@
 import OsmController from "../../controller/osm.controller";
+import indexRoutes from "../../routes/index";
+const supertest = require("supertest");
 const osm = new OsmController();
 
+describe('GET /get-geo-json/:value', function() {
+  let bbox: any = "bbox=0.00000,0.00000,0.00000,0.00000";
+  test('responds with json',  async () =>{
+     supertest(indexRoutes)
+      .get('/get-geo-json/'+bbox)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+  });
+});
+  
 describe("Geo Json Controller", () => {
   let bbox: any = "bbox=0.00000,0.00000,0.00000,0.00000";
   let url: any = `https://www.openstreetmap.org/api/0.6/map?${bbox}`;
