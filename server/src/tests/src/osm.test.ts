bf8f70c6b00718b33bf35ac7618fe2a03b338ea0
Jonas <jonaschinagorom@gmail.com>
2022-09-24 14:50:13 +0200

    supertest

--- server/src/tests/src/osm.test.ts
+++ server/src/tests/src/osm.test.ts
@@ -3,20 +3,10 @@ import indexRoutes from "../../routes/index";
 const supertest = require("supertest");
 const osm = new OsmController();
 
-describe('GET /get-geo-json/:value', function() {
-  let bbox: any = "bbox=0.00000,0.00000,0.00000,0.00000";
-  test('responds with json',  async () =>{
-     supertest(indexRoutes)
-      .get('/get-geo-json/'+bbox)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-});
-  
 describe("Geo Json Controller", () => {
   let bbox: any = "bbox=0.00000,0.00000,0.00000,0.00000";
   let url: any = `https://www.openstreetmap.org/api/0.6/map?${bbox}`;
+
   it("send request to url", async () => {
     const res = await osm.callUrl(url);
     expect(res).resolves;
@@ -25,5 +15,13 @@ describe("Geo Json Controller", () => {
   it("get geo json", async () => {
     const res = await osm.getGeoJSON(url, bbox);
     expect(res).resolves;
+  });
+
+  it("test /get-geo-json/:value", async () => {
+    await supertest(indexRoutes)
+      .get("/get-geo-json/" + bbox)
+      .set("Accept", "application/json")
+      .expect("Content-Type", "application/json")
+      .expect(200).resolves;
   });
 });
